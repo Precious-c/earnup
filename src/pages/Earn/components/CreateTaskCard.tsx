e0c683ac0f0356a6882735a234d56d015331db24
Precious <70088028+Precious-c@users.noreply.github.com>
2025-03-19 10:23:18 +0100

    Update CreateTaskCard.tsx

--- src/pages/Earn/components/CreateTaskCard.tsx
+++ src/pages/Earn/components/CreateTaskCard.tsx
@@ -2,7 +2,7 @@ import createtaskIcon from "@/assets/images/seal.Ct9Xs28w.gif";
 import { ArrowRight } from "lucide-react";
 const CreateTaskCard = () => {
   return (
-    <div className="flex items-center gap-2 rounded-lg w-full bg-[#1C1C1E] text-gray-100 hover:bg-[#2C2C2E] px-4  my-4 mt-2 cursor-pointer ">
+    <div className="flex items-center gap-2 rounded-lg w-full bg-[#1C1C1E] text-gray-100 hover:bg-[#2C2C2E] px-4 py-1 my-4 mt-2 cursor-pointer ">
       <div>
         <img src={createtaskIcon} alt="" className="w-20 h-20" />
       </div>
