84e9e1fda946a77c3e8352ccd3fa82238140f3cf
Precious <70088028+Precious-c@users.noreply.github.com>
2025-03-20 11:46:28 +0100

    Update CreateTaskPage.tsx

--- src/pages/CreateTask/CreateTaskPage.tsx
+++ src/pages/CreateTask/CreateTaskPage.tsx
@@ -121,7 +121,7 @@ export default function CreateTaskPage() {
               Read full rules here <ArrowRight />
             </a>
 
-            <Button size="lg" className="w-full my-6">
+            <Button size="lg" className="w-full my-6 h-14 font-medium text-lg">
               Publish own task
             </Button>
           </div>
